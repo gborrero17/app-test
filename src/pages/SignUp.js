a1b50e40ad21dee90b6c8991f9c2d4f793a49a2e
giselle <giselle@devusol.com>
2022-07-20 17:42:23 -0400

    committed by sean before happy hour

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import GradientSignUp from '../components/gradientSignUp';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { Form, Button } from 'react-bootstrap'
+import { Form } from 'react-bootstrap'
 import {Link} from 'react-router-dom'
 import PrimaryButton from '../components/primaryButton';
 
@@ -12,8 +12,12 @@ const SignUp = () => {
         <div className="form-container">
         <Form className="signup-form">
             <Form.Group>
-                <Form.Control className="name-input" type="text" placeholder="name" name="name"></Form.Control>
-                <Form.Control className="email-input" type="text" placeholder="email" name="email" ></Form.Control>
+            <img src={require("../img/whitehorlogo.png")} alt="Full Name"></img>
+                <Form.Control className="fullname-input" type="text" placeholder="Full Name" name="fullname"></Form.Control>
+                <Form.Control className="email-input" type="text" placeholder="E-Mail" name="email" ></Form.Control>
+                <Form.Control className="password-input" type="text" placeholder="Password" name="password"></Form.Control>
+                <Form.Control className="datebirth-input" type="text" placeholder="Date of Birth" name="datebirth" ></Form.Control>
+                <Form.Control className="zipcode-input" type="text" placeholder="Zipcode" name="zipcode"></Form.Control>
                 <Link to='/SignUp'><PrimaryButton className="submit-button" value="submit" type="submit" text='Sign Up'/></Link>
             </Form.Group>
         </Form>
